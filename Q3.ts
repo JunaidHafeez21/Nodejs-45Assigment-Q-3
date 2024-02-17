c139cda15c86805f7100d3b6be5f00b58ad3294a
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-17 19:18:18 +0500

    Add files via upload

---
+++ Q3.ts
@@ -0,0 +1,11 @@
+// Q3: Name cases: store a person's  name in a varibale, and then print that person's name in lowecase, uppercase, and titlecase.
+
+// let firstName: string = "junaid hafeez";
+
+// console.log(firstName.toLowerCase());
+
+let secondName: string = "junaid hafeez";
+
+console.log(secondName.toLocaleLowerCase());
+console.log(secondName.toLocaleUpperCase());
+console.log(secondName.charAt(3).toUpperCase() + secondName.slice(5));
